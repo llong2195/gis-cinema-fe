915cb3f5eca4c41e2a76479cd917bb2240cd5773
llong2195 <nduylong9501@gmail.com>
2023-01-10 23:19:49 +0700

    hot fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
-import App from "./App";
+import App from "./app";
 import SignIn from "./pages/sign-in";
 const Index = () => (
   <BrowserRouter>
     <Switch>
-        <Route path={"/Map"} component={App} />
+        <Route exact path={"/Map"} component={App} />
         <Route path={"/SignIn"} component={SignIn} />
     </Switch>
   </BrowserRouter>
